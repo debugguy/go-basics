b05f70a2d51ee617de5bc65e88269b4d73bf71a6
lb <liubomyr.baklan@aura.com>
2021-10-26 07:35:30 -0700

    before pause

--- func.go
+++ func.go
@@ -1,19 +1,30 @@
 package main
 
-import "fmt"
-
-const pi = 3.14
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 func main() {
-	printCircleArea()
+	printCircleArea(22)
+	// fmt.Println("Plosha kryga s radiusom 5cm = ", calcCircleArea(5))
 }
 
-func printCircleArea() {
-	circleRadius := 2
-	circleArea := float32(circleRadius) * float32(circleRadius) * pi
+func printCircleArea(radius int) {
+	circleArea, err := calculateCircleArea(radius)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	fmt.Println("Radius:", circleRadius)
+	fmt.Println("Radius kryga:", radius)
 	fmt.Printf("Formula A=pi*r*r\n")
-
-	fmt.Println("Area = ", circleArea)
+	fmt.Printf("Площа круга: %f cm, kv\n", circleArea)
+}
+func calculateCircleArea(radius int) (float32, error) {
+	if radius <= 0 {
+		return float32(0), errors.New("Radius cannot be negative")
+	}
+	return float32(radius) * float32(radius) * math.Pi, nil
 }
